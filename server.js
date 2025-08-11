ce4856ecddc89092b0f871343d603b651d6dc976
Ai-sh-u <aishwaryachelladurairajeshwari@gmail.com>
2025-08-11 18:56:10 +0530

    first commit

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ require("dotenv").config();
 const studentRoutes = require("./routes/studentRoutes");
 
 const app = express();
-app.use(cors());
+app.use(cors({ origin: "*" }));
 app.use(express.json());
 
 // Routes
